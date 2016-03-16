6f6b4ca019a38d1e610d6126f4b1c16742713709
Ivan Arar <ivan.arar@gmail.com>
2016-03-17 00:12:09 +0100

    version 0.2

--- google_vision_detective/cli.py
+++ google_vision_detective/cli.py
@@ -3,21 +3,25 @@ import click
 from PIL import Image
 from PIL import ImageDraw
 
-from features import Face, Label
+from features import Face, Label, Text, Landmark, Logo, SafeSearch, Properties
 from detective import GoogleVisionDetective, Request, DetectiveException
 
 
-@click.group()
-@click.option('-c', '--credentials')
-@click.option('-i', '--input_dir')
+@click.group(help='Detective is a wrapper for Google Vision API')
+@click.option('-c', '--credentials', type=click.STRING, 
+                                     help='Path to your credentials json file')
+@click.option('-i', '--input_dir', type=click.STRING, 
+                                   help='Path to directory where input images will be stored')
 @click.pass_context
 def cli(detective, credentials, input_dir):
     detective.obj = GoogleVisionDetective(credentials=credentials, input_dir=input_dir)
 
 
-@cli.command()
-@click.option('-i', '--input_file')
-@click.option('-m', '--max_results', default=10)
+@cli.command(help='Execute Image Content Analysis on the entire image and return')
+@click.option('-i', '--input_file', type=click.STRING, 
+                                    help='Path to image on the disk or URL')
+@click.option('-m', '--max_results', default=10,
+                                     help='Maximum results per request')
 @click.pass_context
 def labels(detective, input_file, max_results):
     with Request(detective.obj, input_file) as request:
@@ -27,12 +31,15 @@ def labels(detective, input_file, max_results):
     for response in responses:
         for feature in response.features:
             for label in feature.annotations:
-                print('{description} ({score})'.format(**label))
+                click.echo('{description} ({score})'.format(**label))
 
 
-@cli.command()
-@click.option('-i', '--input_file')
-@click.option('-o', '--output_dir')
+@cli.command(help='Detect faces within the image')
+@click.option('-i', '--input_file', type=click.STRING,
+                                    help='Path to image on the disk or URL')
+@click.option('-o', '--output_dir', type=click.STRING,
+                                    help='Path to directory where the image\
+                                          with detected faces will be stored')
 @click.option('-m', '--max_results', default=10)
 @click.pass_context
 def faces(detective, input_file, output_dir, max_results):
@@ -56,7 +63,91 @@ def faces(detective, input_file, output_dir, max_results):
         output_file = output_dir + response.image.name.split('/')[-1]
         im.save(output_file)
 
-        print('Found %s face%s' % (len(feature.annotations), '' if len(feature.annotations) == 1 else 's'))
-        print('Writing to file %s' % output_file)
+        click.echo('Found %s face%s' % (len(feature.annotations), '' if len(feature.annotations) == 1 else 's'))
+        click.echo('Writing to file %s' % output_file)
 
 
+@cli.command(help='Perform Optical Character Recognition (OCR) on text within the image')
+@click.option('-i', '--input_file', type=click.STRING, 
+                                    help='Path to image on the disk or URL')
+@click.option('-m', '--max_results', default=10,
+                                     help='Maximum results per request')
+@click.pass_context
+def text(detective, input_file, max_results):
+    with Request(detective.obj, input_file) as request:
+        request.feature(Text(max_results=max_results))
+    
+    responses = detective.obj.detect()
+    for response in responses:
+        for feature in response.features:
+            for label in feature.annotations:
+                click.echo('{description} ({locale})'.format(**label))
+
+
+@cli.command(help='Detect geographic landmarks within the image')
+@click.option('-i', '--input_file', type=click.STRING, 
+                                    help='Path to image on the disk or URL')
+@click.option('-m', '--max_results', default=10,
+                                     help='Maximum results per request')
+@click.pass_context
+def landmark(detective, input_file, max_results):
+    with Request(detective.obj, input_file) as request:
+        request.feature(Landmark(max_results=max_results))
+    
+    responses = detective.obj.detect()
+    for response in responses:
+        for feature in response.features:
+            for label in feature.annotations:
+                #click.echo('{description} ({locale})'.format(**label))
+                click.echo(label)
+
+
+@cli.command(help='Detect company logos within the image')
+@click.option('-i', '--input_file', type=click.STRING, 
+                                    help='Path to image on the disk or URL')
+@click.option('-m', '--max_results', default=10,
+                                     help='Maximum results per request')
+@click.pass_context
+def logo(detective, input_file, max_results):
+    with Request(detective.obj, input_file) as request:
+        request.feature(Logo(max_results=max_results))
+    
+    responses = detective.obj.detect()
+    for response in responses:
+        for feature in response.features:
+            for label in feature.annotations:
+                #click.echo('{description} ({locale})'.format(**label))
+                click.echo(label)
+
+
+@cli.command(help='Determine image safe search properties on the image')
+@click.option('-i', '--input_file', type=click.STRING, 
+                                    help='Path to image on the disk or URL')
+@click.option('-m', '--max_results', default=10,
+                                     help='Maximum results per request')
+@click.pass_context
+def safe_search(detective, input_file, max_results):
+    with Request(detective.obj, input_file) as request:
+        request.feature(SafeSearch(max_results=max_results))
+    
+    responses = detective.obj.detect()
+    for response in responses:
+        for feature in response.features:
+            for key, val in feature.annotations.items():
+                click.echo('{}: {})'.format(key, val))
+
+
+@cli.command(help='Compute a set of properties about the image (such as the image\'s dominant colors)')
+@click.option('-i', '--input_file', type=click.STRING, 
+                                    help='Path to image on the disk or URL')
+@click.option('-m', '--max_results', default=10,
+                                     help='Maximum results per request')
+@click.pass_context
+def properties(detective, input_file, max_results):
+    with Request(detective.obj, input_file) as request:
+        request.feature(Properties(max_results=max_results))
+    
+    responses = detective.obj.detect()
+    for response in responses:
+        for feature in response.features:
+            click.echo(feature.annotations)
--- google_vision_detective/features.py
+++ google_vision_detective/features.py
@@ -12,7 +12,10 @@ class Feature(object):
         for method in dir(self):
             obj_param = getattr(self, method)
             if type(obj_param) is Field:
-                setattr(self, method, response[obj_param.name])
+                try:
+                    setattr(self, method, response[obj_param.name])
+                except:
+                    setattr(self, method, [])
         return self
 
     @classmethod
@@ -36,3 +39,48 @@ class Face(Feature):
         name = 'faces'
         description = 'Face Detection'
         detection_type = 'FACE_DETECTION'
+
+
+class Text(Feature):
+    annotations = Field('textAnnotations')
+
+    class Meta:
+        name = 'text'
+        description = 'Text Detection'
+        detection_type = 'TEXT_DETECTION'
+
+
+class Landmark(Feature):
+    annotations = Field('landmarkAnnotations')
+
+    class Meta:
+        name = 'landmark'
+        description = 'Landmark Detection'
+        detection_type = 'LANDMARK_DETECTION'
+
+
+class Logo(Feature):
+    annotations = Field('logoAnnotations')
+
+    class Meta:
+        name = 'logo'
+        description = 'Logo Detection'
+        detection_type = 'LOGO_DETECTION'
+
+
+class SafeSearch(Feature):
+    annotations = Field('safeSearchAnnotation')
+
+    class Meta:
+        name = 'safe-search'
+        description = 'Safe Search Detection'
+        detection_type = 'SAFE_SEARCH_DETECTION'
+
+
+class Properties(Feature):
+    annotations = Field('imagePropertiesAnnotation')
+
+    class Meta:
+        name = 'properties'
+        description = 'Image Properties'
+        detection_type = 'IMAGE_PROPERTIES'
